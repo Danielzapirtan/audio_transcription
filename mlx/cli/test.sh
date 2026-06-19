c49c8f8e3de63253ad1322c58aabb842fac7e2b0
root <antoniudanielzapirtan@gmail.com>
2026-06-19 19:26:38 +0300

    Fri Jun 19 19:26:38 EEST 2026

--- mlx/cli/test.sh
+++ mlx/cli/test.sh
@@ -14,7 +14,8 @@ source venv/bin/activate
 export VIRTUAL_ENV
 python$VER -m pip install --upgrade pip
 brew install ffmpeg-full
-pip install -r cli/requirements.txt
+cd mlx/cli
+pip install -r requirements.txt
 if test -z $HF_TOKEN; then
 	echo "exportati HF_TOKEN"
 else
